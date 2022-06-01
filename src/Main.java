f0ad24cfd3a6b4f48416f14f85ac981f059476a5
wucyang <wucyang@chint.com>
2022-06-01 16:28:22 +0800

    添加属性枚举类

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,23 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        PokeMon inban = new PokeMon();
-        PokeMon huo = new PokeMon();
-        PokeMon shui = new PokeMon();
-        PokeMon dian= new PokeMon();
-        PokeMon cao = new PokeMon();
-        PokeMon bing = new PokeMon();
-        PokeMon gedou = new PokeMon();
-        PokeMon du = new PokeMon();
-        PokeMon dimian  = new PokeMon();
-        PokeMon feixing = new PokeMon();
-        PokeMon chaonengli = new PokeMon();
-        PokeMon chong = new PokeMon();
-        PokeMon yanshi = new PokeMon();
-        PokeMon youling = new PokeMon();
-        PokeMon loong = new PokeMon();
+        while(true) {
+            Scanner input=new Scanner(System.in);
+            String shuxing = input.next();
+            if(shuxing == "exit") {
+                break;
+            }
+            String res = fun(shuxing);
+            System.out.println(res);
+        }
+    }
 
-        inban.setShuxing(Illustrated.YIBAN.getShuxing());
-        List<PokeMon> inbanIA = Arrays.asList(youling);
+    public static String fun(String shuxing){
 
+        return shuxing;
     }
 }
--- src/PokeMon.java
+++ src/PokeMon.java
@@ -1,68 +1,133 @@
 import java.util.List;
 
-public class PokeMon {
-    private List<PokeMon> doubleAtt;
-    private List<PokeMon> doubleDen;
-    private List<PokeMon> halfAtt;
-    private List<PokeMon> halfDen;
-    private List<PokeMon> invalidAtt;
-    private List<PokeMon> invalidDen;
-    private String shuxing;
+public enum PokeMon {
+    /**
+     * 一般系
+     */
+    YIBAN("无","格斗","岩石","钢","幽灵","幽灵"),
 
-    public String getShuxing() {
-        return shuxing;
-    }
+    /**
+     * 火系
+     */
+    HUO("草,冰,虫,钢","水,地面,岩石","火,水,岩石,龙","火,草,冰,虫,钢,妖精","无","无"),
 
-    public void setShuxing(String shuxing) {
-        this.shuxing = shuxing;
-    }
+    /**
+     * 水系
+     */
+    SHUI("火,地面,岩石","电,草","水,草,龙","火,水,冰,钢","无","无"),
 
-    public List<PokeMon> getInvalidAtt() {
-        return invalidAtt;
-    }
+    /**
+     * 电系
+     */
+    DIAN("水,飞行","地面","电,草,龙","电,飞行,钢","地面","无"),
 
-    public void setInvalidAtt(List<PokeMon> invalidAtt) {
+    /**
+     * 草系
+     */
+    CAO("水,地面,岩石","火,冰,毒,飞行,虫","火，草，毒，飞行，虫，龙，钢","水,地面,岩石","无","无"),
+
+    /**
+     * 冰系
+     */
+    BING("草,地面,飞行,龙","火,格斗,岩石,钢","火,水,冰,钢","冰","无","无"),
+
+    /**
+     * 格斗系
+     */
+    GEDOU("一般,冰,岩石,恶,钢","飞行,超能力,妖精","","","",""),
+
+    /**
+     * 毒系
+     */
+    DU("","","","","",""),
+
+    /**
+     * 地面系
+     */
+    DIMIAN("","","","","",""),
+
+    /**
+     * 飞行系
+     */
+    FEIXING("","","","","",""),
+
+    /**
+     * 超能系
+     */
+    CHAONENG("","","","","",""),
+
+    /**
+     * 虫系
+     */
+    CHONG("","","","","",""),
+
+    /**
+     * 岩石系
+     */
+    YANSHI("","","","","",""),
+
+    /**
+     * 幽灵系
+     */
+    YOULING("","","","","",""),
+
+    /**
+     * 龙系
+     */
+    LOONG("","","","","",""),
+
+    /**
+     * 恶系
+     */
+    EE("","","","","",""),
+
+    /**
+     * 钢系
+     */
+    GANG("","","","","",""),
+
+    /**
+     * 妖精系
+     */
+    YAOJING("","","","","","");
+    private String doubleAtt;
+    private String doubleDen;
+    private String halfAtt;
+    private String halfden;
+    private String invalidAtt;
+    private String invalidDen;
+
+    PokeMon(String doubleAtt,String doubleDen,String halfAtt,String halfden,String invalidAtt,String invalidDen)
+    {
+        this.doubleAtt = doubleAtt;
+        this.doubleDen = doubleAtt;
+        this.halfAtt = halfAtt;
+        this.halfden = halfden;
         this.invalidAtt = invalidAtt;
-    }
-
-    public List<PokeMon> getInvalidDen() {
-        return invalidDen;
-    }
-
-    public void setInvalidDen(List<PokeMon> invalidDen) {
         this.invalidDen = invalidDen;
-    }
 
-    public List<PokeMon> getDoubleAtt() {
+    }
+    public String getDoubleAtt() {
         return doubleAtt;
     }
 
-    public void setDoubleAtt(List<PokeMon> doubleAtt) {
-        this.doubleAtt = doubleAtt;
-    }
-
-    public List<PokeMon> getDoubleDen() {
+    public String getDoubleDen() {
         return doubleDen;
     }
 
-    public void setDoubleDen(List<PokeMon> doubleDen) {
-        this.doubleDen = doubleDen;
-    }
-
-    public List<PokeMon> getHalfAtt() {
+    public String getHalfAtt() {
         return halfAtt;
     }
 
-    public void setHalfAtt(List<PokeMon> halfAtt) {
-        this.halfAtt = halfAtt;
+    public String getHalfden() {
+        return halfden;
     }
 
-    public List<PokeMon> getHalfDen() {
-        return halfDen;
+    public String getInvalidAtt() {
+        return invalidAtt;
     }
 
-    public void setHalfDen(List<PokeMon> halfDen) {
-        this.halfDen = halfDen;
+    public String getInvalidDen() {
+        return invalidDen;
     }
-
 }
